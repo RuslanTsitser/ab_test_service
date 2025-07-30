44396705ef6704b6512d5396e340d252ed818ffe
RuslanTsitser <cicer_ruslan@mail.ru>
2025-07-30 19:30:54 +0300

    refactor: simplify version tagging in script by removing annotation and using push --tags

--- new_version.sh
+++ new_version.sh
@@ -2,5 +2,5 @@ version=$(cat pubspec.yaml | grep "version:" | awk '{print $2}')
 
 echo "Current version: $version"
 
-git tag -a "v$version" -m "v$version"
-git push origin "v$version"
+git tag "$version"
+git push --tags
